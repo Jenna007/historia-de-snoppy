e3920c5808e6ad210e3ed94fd30cdc390a720ec3
Jenna 007 <nadiveron12@gmail.com>
2024-11-26 13:00:46 -0300

    archivos de la pagina

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Index from './components/Index';
+
+function App() {
+  return (
+    <div className="h-screen bg-white font-serif">
+<Index></Index>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Index = () => {
+    return (
+        <div className="container text-lg">
+            <h1 class="text-4xl font-bold">Snoppy el perro mas feliz</h1>
+            <div className="columns">
+                <div className="column">
+                <img src="https://i.pinimg.com/474x/1d/be/c1/1dbec134483b059842ecaf3a0644af27.jpg" alt="" />
+                </div>
+                <div className="column">
+                <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExbjlldjlxdHJ2Y2FsZTJ2dWxtNm94cmpncDZ6M2hjbW81em81MWhpZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1y4P80pt9Uh3yWbYri/giphy.gif" alt="" />
+                </div>
+            </div>
+
+            <p>
+                Soy Snoopy, un perro beagle que vive con mi dueño, Charlie Brown. Desde que era un cachorro, siempre he tenido una gran imaginación. Aunque a veces me veo como un perro común, mi mente vuela a lugares muy lejanos. A menudo me encuentro luchando como un piloto de guerra en el aire, enfrentándome al Barón Rojo, o escribiendo mi gran novela, aunque no siempre sé cómo terminarla.
+            </p>
+            <p>
+                Me encanta pasar el tiempo en mi casita, donde me retiro para pensar o simplemente relajarme. A pesar de mis fantasías, siempre tengo un amor incondicional por la comida y me gusta disfrutar de la compañía de mis amigos, como Woodstock, mi pequeño compañero. A veces, mis aventuras son un poco solitarias, pero me gusta pensar que, en algún modo, todos compartimos este mundo lleno de posibilidades.
+            </p>
+        </div>
+    )
+}
+
+export default Index;
